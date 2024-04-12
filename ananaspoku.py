c0d4273db6e75c6c157bf1ce5abcf496aeb0c15d
Sirrein16 <166790549+Sirrein16@users.noreply.github.com>
2024-04-12 11:33:37 +0500

    Update ananaspoku.py

--- ananaspoku.py
+++ ananaspoku.py
@@ -1,1 +1,9 @@
-print('RUSLAN IDI V ARBUZ I NE BUDU TEBYA LOMAT')
+
+def ses():
+    sas = 1
+    game = input('wevhrvkwevhv')
+    due = input('238897769463991684781346')
+    due = int(input('238897769463991684781346'))
+    return (a + b)
+    break()
+    a = int(input('123')
