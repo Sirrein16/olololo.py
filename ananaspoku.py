44e0d0c70598114f2e9db8956887a7b7f85b4fd2
platonl0x <166791360+platonl0x@users.noreply.github.com>
2024-04-12 11:31:01 +0500

    Update ananaspoku.py

--- ananaspoku.py
+++ ananaspoku.py
@@ -1,12 +1,1 @@
-from PyQt5 import*
-print('sus')
-def ses():
-    sas = 1
-    game = input('wevhrvkwevhv')
-    due = int(input('238897769463991684781346'))
-    return (a + b)
-    break()
-    a = int(input('123')
-    int(insert('sewerg4w3G23G2gg'))
-print('ses')
-    
+print('RUSLAN IDI V ARBUZ I NE BUDU TEBYA LOMAT')
