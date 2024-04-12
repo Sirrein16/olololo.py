9e336acb7fe5ef981d8b7c6972af84084c186ea8
Sirrein16 <166790549+Sirrein16@users.noreply.github.com>
2024-04-12 11:28:23 +0500

    Update ananaspoku.py

--- ananaspoku.py
+++ ananaspoku.py
@@ -1,11 +1,11 @@
 from PyQt5 import*
 print('sus')
 def ses():
-    sas = 1:
+    sas = 1
     game = input('wevhrvkwevhv')
     due = input('238897769463991684781346')
     return (a + b)*c
-    break
+    break()
     a = int(input('123')
     int(insert('sewerg4w3G23G2gg'))
 print(ses)
