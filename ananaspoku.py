ab9f7e588bb295cb9365136b00c6796492dd3012
Sirrein16 <166790549+Sirrein16@users.noreply.github.com>
2024-04-12 11:15:51 +0500

    Create ananaspoku.py

---
+++ ananaspoku.py
@@ -0,0 +1,11 @@
+print(sus)
+def ses()
+sas = 1:
+int(input('wevhrvkwevhv')
+input('238897769463991684781346')
+    retuer (a + b)*c
+    braik
+    import pygame.import
+    a = in(in(uppud()"afs00")
+     dint isart(sewerg4w3G23G2gg)
+    
