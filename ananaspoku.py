c5138886ac2ea87d91f015f206d00291d8c1cb9f
platonl0x <166791360+platonl0x@users.noreply.github.com>
2024-04-12 11:25:54 +0500

    Update ananaspoku.py

--- ananaspoku.py
+++ ananaspoku.py
@@ -1,11 +1,12 @@
-print(sus)
-def ses()
-sas = 1:
-int(input('wevhrvkwevhv')
-input('238897769463991684781346')
-    retuer (a + b)*c
-    braik
-    import pygame.import
-    a = in(in(uppud()"afs00")
-     dint isart(sewerg4w3G23G2gg)
+from PyQt5 import*
+print('sus')
+def ses():
+    sas = 1:
+    game = input('wevhrvkwevhv')
+    due = input('238897769463991684781346')
+    return (a + b)*c
+    break
+    a = int(input('123')
+    int(insert('sewerg4w3G23G2gg'))
+print(ses)
     
