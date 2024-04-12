54822ea0add935293ce473fb684623083450858f
Sirrein16 <166790549+Sirrein16@users.noreply.github.com>
2024-04-12 11:29:59 +0500

    Update ananaspoku.py

--- ananaspoku.py
+++ ananaspoku.py
@@ -3,7 +3,7 @@ print('sus')
 def ses():
     sas = 1
     game = input('wevhrvkwevhv')
-    due = input('238897769463991684781346')
+    due = int(input('238897769463991684781346'))
     return (a + b)
     break()
     a = int(input('123')
